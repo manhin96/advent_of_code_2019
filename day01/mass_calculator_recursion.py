4fa095782cc6e8e8dbd40f3d018cac0325271201
manhin <manhinto@hotmail.com>
2019-12-02 23:44:01 +0100

    day one: part two recursion

---
+++ day01/mass_calculator_recursion.py
@@ -0,0 +1,19 @@
+import math
+
+def mass_fuel_calculator(input, totalSum=0):
+    calculatedValue = math.floor(input / 3) - 2
+    if calculatedValue <= 0:
+        return totalSum
+    else:
+        return mass_fuel_calculator(calculatedValue, totalSum + calculatedValue)
+
+# Main method
+
+filepath = "inputList"
+totalFuel = 0
+
+with open(filepath) as fp:
+    for line in fp:
+        totalFuel += mass_fuel_calculator(int(line))
+
+print(int(totalFuel))
